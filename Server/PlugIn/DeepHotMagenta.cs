5dc6c2add48b6aa5e396fc744735640a66a9e6dd
ILAZAR <ILAZAR@gmail.com>
2010-12-17 19:21:43 +0000

    added deep hot magenta

---
+++ Server/PlugIn/DeepHotMagenta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Server.API;
+using PlugIn.Bidders;
+
+namespace PlugIn
+{
+    class DeepHotMagenta: LegoPlayer
+    {
+        public DeepHotMagenta()
+            :base(new SmartBidder(), new PlugIn.CardExchangers.MaximizeTricksBidCardExchanger(), new PlugIn.Gamers.StrongerCardGamer())
+        {
+        }
+
+        public override string Name
+        {
+            get { return "Hot Magenta"; }
+        }
+    }
+}
