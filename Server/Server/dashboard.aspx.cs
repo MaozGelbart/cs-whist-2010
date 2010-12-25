bfc2f8f3f5bcb24b1bcc32cb8479e6c93001a360
ishaijaffe84@gmail.com <ishaijaffe84@gmail.com@22213fd3-82d5-afe8-e4d7-286ab3f5dbb2>
2010-12-25 17:06:57 +0000

    added dashboard.aspx

---
+++ Server/Server/dashboard.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Brain;
+
+namespace Server
+{
+    public partial class dashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var games = (from g in GameFactory.Games
+                         select new
+                         {
+                             Name = g.Name,
+                             Player1Name = g.GetGameStatus().PlayerNames[0],
+                             Player2Name = g.GetGameStatus().PlayerNames[1],
+                             Player3Name = g.GetGameStatus().PlayerNames[2],
+                             Player4Name = g.GetGameStatus().PlayerNames[3],
+                             CurrentRound = g.GetGameStatus().RoundNumber + 1,
+                             StartedAt = g.TimeStarted
+                         }).ToArray();
+            gv_games.DataSource = games;
+            gv_games.DataBind();
+        }
+    }
+}
