a2390de433304f16ae7d6b5e02a7f744f7ff6a63
juggnut <juggnut@gmail.com>
2010-12-25 18:11:44 +0000

    Brainiac implementation of our best current player

---
+++ Server/PlugIn/Brainiac.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Server.API;
+using PlugIn.Bidders;
+
+namespace PlugIn
+{
+    class Brainiac : LegoPlayer
+    {
+        public Brainiac()
+            : base(new SmartBidder(), new PlugIn.CardExchangers.MaximizeTricksBidCardExchanger(), new PlugIn.Gamers.SmartGamer())
+        {
+        }
+
+        public override string Name
+        {
+            get { return "Brainiac"; }
+        }
+    }
+}
