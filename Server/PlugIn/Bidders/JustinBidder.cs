64b9399056bc76d7088e2b3c5109edd71eaa4a49
juggnut@gmail.com <juggnut@gmail.com@22213fd3-82d5-afe8-e4d7-286ab3f5dbb2>
2010-12-27 23:32:24 +0000

    Now he won't make problems when bidding

--- Server/PlugIn/Bidders/JustinBidder.cs
+++ Server/PlugIn/Bidders/JustinBidder.cs
@@ -18,6 +18,23 @@ namespace PlugIn.Bidders
 
         public override int RequestDeclare()
         {
+            int bidders = 0, total = 0;
+
+            for (PlayerSeat p = PlayerSeat.West; p <= PlayerSeat.East; p++)
+            {
+                if (CurrentRoundStatus.Biddings[(int)p].HasValue)
+                {
+                    total += CurrentRoundStatus.Biddings[(int)p].Value.Amount;
+                    bidders++;
+                }
+            }
+
+            if (bidders == 3 && total == 13)
+            {
+                return 1;
+            }
+
+            
             return 0;
         }
     }
